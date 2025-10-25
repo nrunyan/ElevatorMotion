cbfaf07d24c6bbb4bc71c6a32071143bd37dd79e
Natalie Runyan <nrunyan@unm.edu>
2025-10-25 13:25:16 -0600

    Initial push

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        int i = topAlignment();
+        System.out.println(i);
+    }
+
+    public static Integer topAlignment(){
+        return null;
+    }
 }
---
+++ src/MotionAPI.java
@@ -0,0 +1,18 @@
+public class MotionAPI {
+
+    public void setDirection(Direction direction){
+        int i =topAlignment();
+    }
+    public Integer topAlignment(){
+        return null;
+    }
+    public Integer bottomAlignment(){
+        return null;
+    }
+    public void start(){
+
+    }
+    public void stop(){
+
+    }
+}
