6e9939cfddaa7f23e5dbd4bddc1f3b2e0309da74
Daniel Thompson <dthompson1@unm.edu>
2025-10-28 19:58:41 -0600

    sens

--- src/GUI/SensorFX.java
+++ src/GUI/SensorFX.java
@@ -1,4 +1,25 @@
 package GUI;
 
-public class SensorFX {
+import Util.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import Hardware.*;
+
+public class SensorFX implements Observer {
+    private Circle sen;
+
+    @Override
+    public void update(Observable viewee) {
+        if(viewee instanceof Sensor){
+            if(((Sensor) viewee).is_triggered()){
+                sen.setFill(Color.GREEN);
+            }else {
+                sen.setFill(Color.RED);
+            }
+        }
+    }
+
+    public void setSen(Circle sen) {
+        this.sen = sen;
+    }
 }
