fbba6588dc4a0b29b95187e9a6a44a1ebcd22cbe
valerie <vbarker@unm.edu>
2025-10-29 21:02:23 -0600

    TEST FILES TEST FILES TESTING TESTING FILES FILES FILES

--- src/Tests/MotionSimTest.java
+++ src/Tests/MotionSimTest.java
@@ -1,13 +1,24 @@
+package Tests;
+
 import GUI.ElevatorGUI;
+import Hardware.Elevator;
+import Hardware.Motor;
 import Simulation.MotionSimulation;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Tests extends Application {
+public class MotionSimTest extends Application {
+    Motor test_motor;
 
     @Override
     public void start(Stage primaryStage) {
         MotionSimulation sim = new MotionSimulation();
+        test_motor = sim.getMotor();
+
+        MotorControllerTest motorGuy = new MotorControllerTest(test_motor);
+        Thread motorControllerGuyThread = new Thread(motorGuy);
+        motorControllerGuyThread.start();
+
         Thread simThread = new Thread(sim);
         simThread.start();
 
